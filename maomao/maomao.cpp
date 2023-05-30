586355de80d9eb7d1311f8e22da112f325f75c66
Silvania Alves Oliveira <95646237+BangtanBTS@users.noreply.github.com>
2023-05-30 11:44:24 -0300

    Create maomao.cpp

---
+++ maomao/maomao.cpp
@@ -0,0 +1,12 @@
+//Silvania Alves Oliveira
+#include <iostream>
+#include "ListCDE.h"
+#include "pilha.h"
+#include "jogador.h"
+//#include "ClassList.h"
+//#include "ClassCarta.h"
+using namespace std;
+
+int main(){
+	
+}

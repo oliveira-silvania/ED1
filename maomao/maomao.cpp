893254f27f3229792a970c23a7dd51e8ed65e5b1
Silvania Alves Oliveira <95646237+BangtanBTS@users.noreply.github.com>
2023-05-31 16:32:36 -0300

    Update maomao.cpp

--- maomao/maomao.cpp
+++ maomao/maomao.cpp
@@ -1,5 +1,5 @@
 //Silvania Alves Oliveira
-#include <iostream>
+#include <bits/stdc++.h>
 #include "listCDE.h"
 #include "pilha.h"
 #include "carta.h"
@@ -7,9 +7,6 @@
 using namespace std;
 
 
-struct jogador{
-	int id;	
-};
 int main(){
 	carta aux;
 	int numPartidas, numJog;
@@ -17,17 +14,28 @@ int main(){
 	cin>>numPartidas;
 	for(int p=1; p<=numPartidas; p++){ //numero de rodadas
 		cin>>numJog; 
-		Lista jg[numJog];
-		listCDE<jogador> vez[numJog]; //vez do jogador jogar 
-		stack<carta> monte, lixo;
+		listCDE<int> Jog;
+		vector<Lista> posJog(numJog+1);
+		Stack<carta> monte, lixo;
 		for(int c=0; c<104; c++){ //colocar as cartas na pilha monte
 			cin>>aux.valor>>aux.naipe;
 			monte.Push(aux);
 		}
-		for(int j=0; j<numJog; j++){ 
-			for(int k=0; k<=5; k++){ //destribuir as cartas para cada jogador
-				jg[j].InsertL(monte.Top());
+		for(int j=1; j<=numJog; j++){ //destribuir as cartas para cada jogador
+			for(int k=0; k<=5; k++){ 
+				posJog[j].InsertL(monte.Top());
 				monte.Pop();
+			}
+		}
+		for(int i=1; i<=numJog; i++){
+			Jog.InsertNext(i);
+		}
+		int Pont = Jog.ItRet();
+		bool sentido=true;
+		while(numJog>1){
+			bool acabouRodada=false;
+			if(lixo.Empty()){
+				lixo.Push(posJog[Pont].primeiraCarta());
 			}
 		}
 	}

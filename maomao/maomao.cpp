2802aa54d96b7a02964bed1d00138fe59f31f216
Silvania Alves Oliveira <95646237+BangtanBTS@users.noreply.github.com>
2023-06-02 11:38:15 -0300

    Update maomao.cpp

--- maomao/maomao.cpp
+++ maomao/maomao.cpp
@@ -3,7 +3,7 @@
 #include "listaCDE.h"
 #include "pilha.h"
 #include "carta.h"
-#include "list.h"
+#include "lista.h"
 using namespace std;
 
 
@@ -18,28 +18,33 @@ int main(){
 		listCDE<int> Jog;
 		Stack<carta> monte, lixo;
 		int continua=numJog-1;
+		
 		while(continua--){
 			for(int c=0; c<104; c++){ //colocar as cartas na pilha monte
 				cin>>aux.valor>>aux.naipe;
 				monte.Push(aux);
+				
 			}
 			for(int j=1; j<=numJog; j++){ //destribuir as cartas para cada jogador
-				for(int k=0; k<=5; k++){ 
+				
+				for(int k=1; k<=5; k++){ 
 					posJog[j].InsertL(monte.Top());
 					monte.Pop();
 				}
 			}
+			
 			for(int i=1; i<=numJog; i++){
 				Jog.InsertNext(i); //preenchendo a lista circular com a quant de jog
 			}
-			//bool sentido=true;
 			if(lixo.Empty()){ //vai jogar a primeira carta da partida
 				lixo.Push(posJog[Jog.ItRet()].primeiraCarta());
 				Jog.Itpp();
 			}
+			
 			bool zero=true;
 			bool ok=false;
 			while(zero){ //ninguem chegou a zero
+				
 				int sentido=1; //1 horario ; -1 anti horario
 				int Pont = Jog.ItRet(); //retorna a carta que está no vector e que o lista tá apontando *****
 				carta aux;
@@ -85,7 +90,7 @@ int main(){
 							}
 						}
 					}
-					if(sentido=-1){//jogo ta no sentido anti horario
+					if(sentido==-1){//jogo ta no sentido anti horario
 						if(aux.valor=='A'){//proximo jog não jogar
 							Jog.Itmm();
 							Jog.Itmm();
@@ -127,6 +132,7 @@ int main(){
 					lixo.Top()=inverte;
 				}
 				if(posJog[Pont].SizeL()==0) zero=false;//verificar se algum jogador tem size==0	
+				
 			}
 			//verificar quem perdeu
 		}
